65f87aea3107f0d174ee3dad4da4e93700152804
Aquib Shaikh <105275742+Aquibshaikh313@users.noreply.github.com>
2025-09-12 20:19:51 +0530

    Delete 10 jo dikhta hai wo bikta hai directory

--- 10 jo dikhta hai wo bikta hai/Theory/Coding/Components/components/Grocery.js
+++
@@ -1,7 +0,0 @@
-
-const Grocery = () => {
- return (
-  <h1>This is our seperate Grocery Component page</h1>
- )
-}
-export default Grocery;
--- 10 jo dikhta hai wo bikta hai/Theory/Coding/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/";
-
-export const  LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
-
-export const MENU_API_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
