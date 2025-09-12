ed43f944424a75cc3e9f91de602cccb274df0bea
Aquib Shaikh <105275742+Aquibshaikh313@users.noreply.github.com>
2025-09-12 18:28:37 +0530

    Add files via upload
    Completed 10 lecture addedtailwind css

---
+++ 10 jo dikhta hai wo bikta hai/Theory/Coding/Components/components/Contact.js
@@ -0,0 +1,12 @@
+// rfce --> shortcut for creating the component
+//dont use the above thing as a beginner
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us Page</h1>
+
+    </div>
+  )
+}
+export default Contact;
